521e2eff848fcc6073ec417cca7abdc17a0ad68d
Ralph Kazemier <ralph.kazemier@gmail.com>
2018-12-16 15:52:01 -0600

    Improving highres image support

--- src/vcl-comps/devtools/Editor$/md.js
+++ src/vcl-comps/devtools/Editor$/md.js
@@ -1,13 +1,36 @@
-"markdown";
+"on, markdown";
+var on = require("on");
 var markdown = require("markdown");
 
 function render() {
 	var root = markdown.toHTMLTree(this.getValue());
 	this.up().vars("root", markdown.toHTMLTree(this.getValue()));//[].concat(root));
-    this.up().qsa("#output").forEach(_ => _.setContent(markdown.renderJsonML(root)));
+    this.up().qsa("#output").forEach(_ => {
+    	_.setContent(markdown.renderJsonML(root));
+    	_.update(function() {
+		    on(this._node.qsa("img"), "load", function(img) {
+		    	img = this;
+		    	console.log(img.naturalWidth, img);
+		    	if(img.src.indexOf("?2x") !== -1) {
+		    		img.style.width = img.naturalWidth / 2 + "px";
+		    	}
+		    });
+    	}.bind(_));
+    });
+    
 }        	
 
-$([
+var Handlers = {
+	"#output onDblClick": function(evt) {
+    	if(evt.target.nodeName === "IMG" && evt.shiftKey === true) {
+    		var img = evt.target; 
+    		img.style.maxWidth = img.style.maxWidth ? "" : img.naturalWidth / 2 + "px";
+    		this.app().qs("#console #console").print(evt.target.src);
+    	}
+	}
+};
+
+$([], { handlers: Handlers }, [
     $i("ace", { 
     	align: "left", width: 475, action: "toggle-source",
     	executesAction: "none",
@@ -33,6 +56,7 @@ $([
 	    "font-size": "12pt",
 	    padding: "10px",
 	    "img:not(:hover)": "max-width: 75%; max-height: 600px;",
-	    "code": "border-radius:3px;font-size: 10pt;background-color:white;padding:2px;line-height:12pt;"
+	    // "img:hover": "width:100%;max-width:600px;",
+	    "code": "border-radius:3px;font-size: 10pt;background-color:white;padding:2px;line-height:12pt;",
     } })
 ]);
