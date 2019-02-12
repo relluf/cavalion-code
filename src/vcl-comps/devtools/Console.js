74fc5ae24b5815ed3c438ce40b7aa5a0113a4af3
Ralph Kazemier <ralph.kazemier@gmail.com>
2019-02-11 20:14:31 -0600

    implementing print()

--- src/vcl-comps/devtools/Console.js
+++ src/vcl-comps/devtools/Console.js
@@ -1,3 +1,10 @@
 $("vcl/ui/Form", { activeControl: "console" }, [
-    $("vcl/ui/Console#console", { align: "client", classes: "no-time" })
+    $("vcl/ui/Console#console", { align: "client", classes: "no-time",
+    	onLoad: function() {
+    		var me = this;
+			this._owner.print = function() {
+				return me.print.apply(me, arguments);
+			};
+    	}
+    })
 ]);
