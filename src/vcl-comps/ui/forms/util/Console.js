1457ff7d9e4b25a64132c00001ad1dd08bda6895
Ralph Kazemier <ralph.kazemier@gmail.com>
2019-04-13 14:00:06 -0500

    adding setPaths

--- src/vcl-comps/ui/forms/util/Console.js
+++ src/vcl-comps/ui/forms/util/Console.js
@@ -2,37 +2,36 @@
 
 var js = require("js");
 
-function req() {
-	if(arguments.length == 1) {
-    	try {
-    		return require(arguments[0]);
-    	} catch(e) {}
-	}
-    var d = new Deferred();
-    require.apply(this, [js.copy_args(arguments),
-        function () {
-            d.callback.apply(d, js.copy_args(arguments));
-        },
-        function (err) {
-            d.errback(err);
-        }
-    ]);
-    return d;
+function setPaths() {
+	localStorage.setItem("cavalion-js-path", "/home/Workspaces/cavalion.org/cavalion-js/src/");
+	localStorage.setItem("cavalion-vcl-path", "/home/Workspaces/cavalion.org/cavalion-vcl/src/");
+	localStorage.setItem("cavalion-blocks-path", "/home/Workspaces/cavalion.org/cavalion-blocks/src/");
+	localStorage.setItem("cavalion-devtools-path", "/home/Workspaces/cavalion.org/cavalion-devtools/src/");
+	localStorage.setItem("cavalion-ide-path", "/home/Workspaces/cavalion.org/cavalion-ide/src/");
+	localStorage.setItem("veldoffice-js-path", "/home/Workspaces/veldapps.com/veldoffice-js/src/");
+	localStorage.setItem("veldapps-v7-path", "/home/Workspaces/veldapps.com/V7/src/");
 }
 
 $([], {}, [
-	$i("console", {
-		onEvaluate: function (expr) {
-		    var scope = this.scope(), app = this.app(), me = this;
-		    var pr = this.print.bind(this);
-		
-		    function open(uri, opts) {
-		        me.bubble("openform", js.mixIn(js.mixIn(opts || {}),
-		            {uri: uri}));
-		    }
-		
-		    /* jshint evil: true */
-		    return eval(expr);
-		}
-	})	
+    $i("console", {
+        onEvaluate: function (expr) {
+            var scope = this.scope(), me = this;
+
+			var app = this.app();
+            var ws = app.down("devtools/Workspace<>:root:selected:visible");
+            var ace = ws && ws.down("devtools/Editor<>:root:visible #ace");
+            var host = ws && ws.down("devtools/Editor<>:root:visible #host");
+            var root = ace && ace.up().down(":root");
+            var pr = this.print.bind(this);
+
+            function open(uri, opts) {
+                me.bubble("openform", js.mixIn(js.mixIn(opts || {}), {
+                    uri: uri
+                }));
+            }
+
+            /* jshint evil: true */
+            return eval(expr);
+        }
+    })
 ]);
