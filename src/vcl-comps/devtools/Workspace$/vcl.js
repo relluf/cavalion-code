2f89d4480135f8f84104719fda382d0af1e4b1fe
Ralph Kazemier <ralph.kazemier@gmail.com>
2019-06-08 11:58:38 -0500

    addig code/build

--- src/vcl-comps/devtools/Workspace$/vcl.js
+++ src/vcl-comps/devtools/Workspace$/vcl.js
@@ -3,6 +3,13 @@ $(["devtools/Workspace"], {
 		"#navigator favorites": [
 			"Workspaces/cavalion.org/cavalion-js/src",
 			"Workspaces/cavalion.org/cavalion-vcl/src",
+			"Workspaces/cavalion.org/cavalion-js/CHANGELOG.md;js/CHANGELOG.md;File",
+			"Workspaces/cavalion.org/cavalion-vcl/CHANGELOG.md;vcl/CHANGELOG.md;File"
 		]
+	},
+	handlers: {
+		"loaded": function() {
+			this.udown("#workspace-needed").execute("code/build");
+		}
 	}
 });
