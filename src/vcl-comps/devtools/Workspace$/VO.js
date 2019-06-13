177831bacd97a3b331c14eb20c508924f2109eb5
Ralph Kazemier <ralph.kazemier@gmail.com>
2019-06-13 01:10:14 -0500

    let's try to make something Karel friendly

--- src/vcl-comps/devtools/Workspace$/VO.js
+++ src/vcl-comps/devtools/Workspace$/VO.js
@@ -1,1 +1,15 @@
-$(["devtools/Workspace<Veldoffice>"]);
+"use blocks/Blocks";
+
+var B = require("blocks/Blocks");
+
+$(["devtools/Workspace<Veldoffice>"], {
+	handlers: {
+		loaded() {
+			B.instantiate(["veldapps/OpenLayers<PDOK-v2>"]);
+		}
+	}
+}, [
+
+	
+	
+]);
