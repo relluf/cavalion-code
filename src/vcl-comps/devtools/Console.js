4a225cd667c8fa400e75a91a1db4bb7b266b8e43
Ralph Kazemier <ralph.kazemier@gmail.com>
2019-07-03 21:21:31 -0500

    cleaning up

--- src/vcl-comps/devtools/Console.js
+++ src/vcl-comps/devtools/Console.js
@@ -1,37 +1,7 @@
-"use blocks/Factory!Console";
+// "use blocks/Factory!Console";
 
-var Console = require("blocks/Factory!Console");
+// var Console = require("blocks/Factory!Console");
 
-$("vcl/ui/Form", { 
-	activeControl: "console",
-	// onLoad: function() {
-	// 	var console = Console.newInstance(this, this._uri, { loaded: function() {}})
-	// 		.setProperties({
-	// 			align: "client", 
-	// 			classes: "no-time bg-white",
-	// 			parent: this
-	// 		});
-	// 	this.print = function() {
-	// 		return console.print.apply(console, arguments);
-	// 	};
-	// }
-}, [
-	// Console.$("console", {align: "client", classes: "no-time",
- //   	onLoad: function() {
- //   		var me = this;
-	// 		this._owner.print = function() {
-	// 			return me.print.apply(me, arguments);
-	// 		};
-	// 		this.print("this.up()", this.up());
- //   	},
- //   	onEvaluate: function(expr) {
- //   		return eval(expr);
- //   	}
-	// })
-	["vcl/ui/Console", "console", {
-		classes: "no-time",
-		onLoad: function() {
-			this.print("ready", this);
-		}
-	}]
+$("vcl/ui/Form", { activeControl: "console" }, [
+	["vcl/ui/Console", "console", { classes: "no-time" }]
 ]);
