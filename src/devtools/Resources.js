3ee4df11b610f64691b5b5e23ccdb1bb12765d91
Ralph Kazemier <ralph.kazemier@gmail.com>
2019-10-06 02:31:31 -0500

    pouching... (finally)

--- src/devtools/Resources.js
+++ src/devtools/Resources.js
@@ -1,3 +1,43 @@
-define(['devtools/Resources-node'], function(Resources) {
-	return Resources;
+define(['devtools/Resources-node', 'devtools/Resources-pouchdb'], 
+function(ResourcesHttp, ResourcesDB) {
+	return {
+		index: function(uris) {
+			return ResourcesHttp.index(uris);
+		},
+		list: function(uri) {
+			if(uri.startsWith("pouchdb://")) {
+				return ResourcesDB.list(uri.substring("pouchdb://".length));
+			}
+			return ResourcesHttp.list(uri).then(function(res) {
+				// if(uri === "/" || uri === "") {
+				// 	res.push({ uri: "pouchdb://", name: "pouchdb://", type: "Folder", link: false });
+				// }
+				return res;
+			});
+		},
+		get: function(uri) {
+			if(uri.startsWith("pouchdb://")) {
+				return ResourcesDB.get(uri.substring("pouchdb://".length));
+			}
+			return ResourcesHttp.get(uri);
+		},
+		create: function(uri, resource) {
+			if(uri.startsWith("pouchdb://")) {
+				return ResourcesDB.create(uri.substring("pouchdb://".length), resource);
+			}
+			return ResourcesHttp.create(uri, resource);
+		},
+		'delete': function(uri) {
+			if(uri.startsWith("pouchdb://")) {
+				return ResourcesDB.delete(uri.substring("pouchdb://".length));
+			}
+			return ResourcesHttp.delete(uri);
+		},
+		update: function(uri, resource) {
+			if(uri.startsWith("pouchdb://")) {
+				return ResourcesDB.update(uri.substring("pouchdb://".length), resource);
+			}
+			return ResourcesHttp.update(uri, resource);
+		}
+	};
 });
