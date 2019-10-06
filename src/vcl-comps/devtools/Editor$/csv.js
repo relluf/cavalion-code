8a117b0d18243ba9048d584dc4ae9b23fd1c682d
Ralph Kazemier <ralph.kazemier@gmail.com>
2019-10-06 02:32:14 -0500

    added search feature

--- src/vcl-comps/devtools/Editor$/csv.js
+++ src/vcl-comps/devtools/Editor$/csv.js
@@ -11,11 +11,60 @@ $([], {
         tab.on({"resource-loaded": f, "resource-saved": f});
         
         return this.inherited(arguments);
+    },
+    onDispatchChildEvent: function (component, name, evt, f, args) {
+        if (name.indexOf("key") === 0) {
+            var scope = this.scope();
+            // this.app().qs("vcl/ui/Console#console").print(name, {f: arguments.callee, args: arguments});
+			if (component === scope['search-input']) {
+                if ([13, 27, 33, 34, 38, 40].indexOf(evt.keyCode) !== -1) {
+                    var list = scope.list;
+                    if(evt.keyCode === 13 && list.getSelection().length === 0 && list.getCount()) {
+                        list.print(list.getSelection(true));
+                    } else if(evt.keyCode === 27) {
+		                scope['search-input'].setValue("");
+		                scope['search-input'].fire("onChange", [true]); // FIXME
+                    }
+                    if (list.isVisible()) {
+                        list.dispatch(name, evt);
+                    }
+                    evt.preventDefault();
+                }
+            }
+        }
+        return this.inherited(arguments);
+    },
+    handlers: {
+		"#source onFilterObject": function(obj) {
+			var q = this.udown("#search-input").getInputValue().toLowerCase();
+			if(q === "") return false;
+			
+			// var text = obj._text || (obj._text = JSON.stringify(obj).toLowerCase());
+			var text = JSON.stringify(obj).toLowerCase();
+			return !q.split(" ").every(function(term) {
+				return text.includes(term);
+			});
+		},
+		"#search-input onKeyDown": function() {
+		},
+		"#search-input onChange": function() {
+			var source = this.scope().source;
+			this.setTimeout(function() { source.updateFilter(); }, 500);
+		}
     }
 }, [
 	$i("ace", { align: "bottom", height: 200 }),
 	$("vcl/data/Array#source"),
+	
+	$("vcl/ui/Bar", [
+		$("vcl/ui/Input", "search-input", { placeholder: locale("Search.placeholder") }),
+		$("vcl/ui/Element", "status")
+	]),
+	
 	$("vcl/ui/List#list", { css: "background-color: white;", align: "client", autoColumns: true, source: "source",
+		onDblClick: function() {
+			this.print(this.getSelection(true));	
+		},
 		onColumnGetValue: function(column, value, row, source) {
 			var value = this._source._arr[row][column._attribute];
 			if(column.getIndex() === 0) {
