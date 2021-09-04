0036277af97cdf8bf61290b1c556249fec6ea91f
Ralph Kazemier <ralph.kazemier@gmail.com>
2021-09-04 13:18:23 -0500

    cleanup

--- src/vcl-comps/App.js
+++
@@ -1,6 +0,0 @@
-$([], {
-    // onDispatchChildEvent: function(component, name, evt, f, args) {
-    //     console.log(name, arguments);
-    //     return this.inherited(arguments);
-    // }
-}, []);
