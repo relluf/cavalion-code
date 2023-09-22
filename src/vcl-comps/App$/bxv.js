97bf7fccb37dd2bd4efae37acae54fc92811d9d8
Ralph <ralph.kazemier@gmail.com>
2023-09-21 19:40:37 -0600

    build 1.0.195

--- src/vcl-comps/App$/bxv.js
+++ src/vcl-comps/App$/bxv.js
@@ -1,41 +1,27 @@
-"use locale, veldoffice/VO, veldoffice/devtools";
-
-window.VO = require("veldoffice/VO");
+"use locale";
 
 require("locale").mixIn({
 		
-		'*.close':				"Sluiten",
-		'*.refresh':			"Vernieuwen",
-		
-		'Session': {
-			'.title':			"Veldoffice-sessie",
-			
-			'.login':			"Aanmelden",
-			'.logout':			"Afmelden",
-			
-			"-username":		"Gebruikersnaam",
-			"-password":		"Wachtwoord"
-		}
 	});
 
-[["devtools/App<>"], { 
+[["devtools/App<veldoffice>"], { 
 	title: "BXV", 
 	vars: {
 		"markdown-source-intially-hidden": true
 		//default-workspaces not possible yet, because devtools/Main<> defines them
 } }, [
-	[("#window"), {
-		overrides: {
-			// dispatchChildEvent(component, name, evt, f, args) {
-			// 	if(name === "click") {
-			// 		component.print(name, "blocked");
-			// 		return false;
-			// 	}
-			// 	return this.inherited(arguments);
-			// }
-		},
-		css: {
-			// '.{Button}:not(.disabled)': "color:red;"
-		}
-	}]
+	// [("#window"), {
+	// 	overrides: {
+	// 		// dispatchChildEvent(component, name, evt, f, args) {
+	// 		// 	if(name === "click") {
+	// 		// 		component.print(name, "blocked");
+	// 		// 		return false;
+	// 		// 	}
+	// 		// 	return this.inherited(arguments);
+	// 		// }
+	// 	},
+	// 	css: {
+	// 		// '.{Button}:not(.disabled)': "color:red;"
+	// 	}
+	// }]
 ]];
